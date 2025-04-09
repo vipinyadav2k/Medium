aa944e077bc47b97124e8756b5b77220761f0455
vipinyadav2k <vipinyadav2k@gmail.com>
2025-04-09 17:33:53 +0530

    handled security check

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,58 +1,77 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
-
+import { useNavigate } from "react-router-dom";
 
 export interface Blog {
-    "content": string;
-    "title": string;
-    "id": number
-    "author": {
-        "name": string
-    }
+  content: string;
+  title: string;
+  id: number;
+  author: {
+    name: string;
+  };
 }
 
 export const useBlog = ({ id }: { id: string }) => {
-    const [loading, setLoading] = useState(true);
-    const [blog, setBlog] = useState<Blog>();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [blog, setBlog] = useState<Blog>();
 
-    useEffect(() => {
-        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
-            headers: {
-                Authorization: localStorage.getItem("token")
-            }
-        })
-            .then(response => {
-                setBlog(response.data.blog);
-                console.log(response.data.blog);
-                setLoading(false);
-            })
-    }, [id])
+  const token = localStorage.getItem("token");
 
-    return {
-        loading,
-        blog
-    }
+  if (!token) {
+    navigate("/signin");
+    return; // Stop further execution if no token
+  }
 
-}
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+        headers: {
+          Authorization:token,
+        },
+      })
+      .then((response) => {
+        setBlog(response.data.blog);
+        console.log(response.data.blog);
+        setLoading(false);
+      });
+  }, [id]);
+
+  return {
+    loading,
+    blog,
+  };
+};
 export const useBlogs = () => {
-    const [loading, setLoading] = useState(true);
-    const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
-            headers: {
-                Authorization: localStorage.getItem("token")
-            }
-        })
-            .then(response => {
-                setBlogs(response.data.blog);
-                setLoading(false);
-            })
-    }, [])
 
-    return {
-        loading,
-        blogs
-    }
-}
+  const token = localStorage.getItem("token");
+
+
+  if (!token) {
+    navigate("/signin");
+    return; // Stop further execution if no token
+  }
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/api/v1/blog/bulk`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        setBlogs(response.data.blog);
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    loading,
+    blogs,
+  };
+};
